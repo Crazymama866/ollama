89e5c65eac8461838f1c3fc3315a5fadafbe293e
Jeremy <remy415@gmail.com>
2024-02-14 11:52:21 -0500

    Testing alternative dll loading.

--- llm/payload_common.go
+++ llm/payload_common.go
@@ -146,7 +146,7 @@ func nativeInit(workdir string) error {
 	slog.Info(fmt.Sprintf("Dynamic LLM libraries %v", variants))
 	slog.Debug("Override detection logic by setting OLLAMA_LLM_LIBRARY")
 
-	gpu.setDynLibs(availableDynLibs)
+	gpu.SetDynLibs(availableDynLibs)
 
 	return nil
 }
